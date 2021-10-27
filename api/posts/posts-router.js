12212c50aa3c6321f89ffd914b0cb69ba13bf488
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-26 23:19:47 -0400

    GET get comments for post by id: Tests passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -107,3 +107,49 @@ router.put("/:id", (req, res) => {
       });
   }
 });
+
+// ENDPOINT METHOD(DELETE) => REMOVE A POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        success: false,
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      await Post.remove(req.params.id);
+      res.json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "The post could not be removed",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
+// ENDPOINT METHOD(GET) => GET ALL THE COMMENTS ASSOCIATED TO THE POST WITH GIVEN ID
+router.get("/", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified Id does not exist",
+      });
+    } else {
+      const messages = await Post.findPostComments(req.params.id);
+      res.json(messages);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The comments information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
+module.exports = router;
