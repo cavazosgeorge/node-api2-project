68f3dccd0ba23dc96d2954de6dc89caa611d7a21
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-26 20:33:34 -0400

    Server created and running

--- api/server.js
+++ api/server.js
@@ -1,2 +1,20 @@
 // implement your server here
-// require your posts router and connect it here
+const express = require("express");
+const server = express();
+
+const dotenv = require("dotenv");
+
+// SETUP CONFIG FILE VARIABLES
+dotenv.config({
+  path: "./config/config.env",
+});
+
+// BODY PARSER
+server.use(express.json());
+
+// IMPORTING ALL ROUTES
+// const users = require("../routes");
+
+// server.use("/api");
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(
+    `Server is listening on port ${PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
